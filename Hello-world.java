56402a6cda99238932ee3d5307238b49ba70c9c4
oscar0812 <oscar0812torres@gmail.com>
2015-09-25 15:46:07 -0500

    First Commit

---
+++ Hello-world.java
@@ -0,0 +1,14 @@
+//
+
+public class Hello-World{
+
+    public Hello-World()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
